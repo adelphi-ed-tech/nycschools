5cf029af2f3d6eee0e887ff88a6c2d2957c49700
mxc <>
2022-04-06 12:04:31 -0400

    moved OLS node graph to ui package as a function

--- nb/school_data/ui.py
+++ nb/school_data/ui.py
@@ -2,6 +2,13 @@ import pandas as pd
 from IPython.display import Markdown as md
 from decimal import *
 
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import seaborn as sns
+import networkx as nx
+
+import math
+
 def ul(t):
 
     items =  [f"- {i}" for i in t]
@@ -55,6 +62,9 @@ def counter():
 def round_f(f, places):
 
     s = str(round(f, places))
+    if not "." in s:
+        return f
+
     whole, frac = s.split(".")
     if whole == "0":
         whole = ""
@@ -71,3 +81,57 @@ def fmt_pearson(r):
     p = round_f(r['p-val'][0], 3)
     r_val = round_f(r['r'][0], 2)
     return f"r({df})={r_val}, p={p}"
+
+
+def edge_label(p, r):
+    return f"{p}={round_f(r,2)}"
+
+def nice_name(n):
+    return n.replace("_", " ").title()
+
+def plot_model(model):
+
+    cmap = mpl.cm.coolwarm
+    # get the data we need from model
+    dv = model.model.endog_names
+
+    params = list(model.params.index.values[1:])
+    coefs = list(model.params.values[1:],)
+    pvalues = list(model.pvalues.round(3).values[1:])
+
+    nodes =  params + [dv]
+    # all targets point to the dv
+    targets =  [dv for _ in range(len(params))]
+    edges = list(zip(params, targets))
+    colors = coefs + [0]
+
+    weights =  [abs(c) for c in coefs]
+    weighted_edges = list(zip(params, targets, weights))
+
+    max_node = 2000
+    max_size = max(weights)
+    node_size = [math.ceil(max_node * (c/max_size)) for c in weights]
+    node_size.append(  max_node * 2 )
+
+    labels =  dict([(n, nice_name(n)) for n in nodes])
+
+    edge_labels = dict([(x, edge_label(x[0], y)) for x, y in zip(edges, coefs)])
+    draw_model(nodes, node_size, weighted_edges, labels, edge_labels, colors, cmap)
+
+def draw_model(nodes, node_size, edges, labels, edge_labels, colors, cmap):
+
+    G = nx.DiGraph()
+    G.add_nodes_from(nodes)
+    G.add_weighted_edges_from(edges)
+    fig, ax = plt.subplots(figsize=(16,9))
+
+    pos = nx.spring_layout(G, k=3)
+
+    nx.draw(G, pos=pos, ax = ax, with_labels=True,
+            labels=labels, node_color=colors, cmap=cmap, node_size=node_size,
+            linewidths=2, min_source_margin=2, min_target_margin=2, font_size=14)
+
+    nx.draw_networkx_edge_labels(G,pos, ax=ax, edge_labels=edge_labels, label_pos=.5, font_size=12)
+    plt.tight_layout()
+    plt.margins(0.05)
+    plt.show()
