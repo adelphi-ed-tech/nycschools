5a294b9d97d78a8eb74732926b57f57d4951a561
mxc <matt@curinga.com>
2022-08-30 14:06:41 -0400

    initial setup.py for school_data library

---
+++ nb/school_data/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='school_data',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Library for working with NYC Open Data for Public Schools',
+    author='mxc',
+    license='GPL',
+)
