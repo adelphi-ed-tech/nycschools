88944948d8ff9749f0a4b9260b02b83ce7e1e9d3
mxc <matt@curinga.com>
2023-01-31 08:29:42 -0500

    got rid of config file, uses local dir or ENV variable for the data directory

--- nycschools/__init__.py
+++ nycschools/__init__.py
@@ -18,25 +18,14 @@ import os
 import os.path
 import json
 from types import SimpleNamespace
-from platformdirs import PlatformDirs
 from .datasets import urls as __urls
 
-dirs = PlatformDirs("nycschools", "mxc")
 
-__config_file = "nycschools.config"
-__config_paths = [
-    os.path.join(dirs.site_config_dir, __config_file),
-    os.path.join(dirs.user_config_dir, __config_file)
-]
+def get_version():
+    """Returns the version of the nycschools package"""
+    return "0.1.0"
 
-__data_paths = [
-    dirs.site_data_dir,
-    dirs.user_data_dir
-]
-
-
-
-def __read_urls():
+def __read_urls__():
     """Reads the urls from the datasets module"""
     urls = {}
     for k,v in __urls.items():
@@ -46,119 +35,36 @@ def __read_urls():
 
 def get_config():
     """initializes configuration settings.
-    First, looks for an environment variable called NYC_SCHOOLS_DATA_DIR
-    NYC_SCHOOLS_DATA_DIR is the path to a read/write directory where data files
-    can be stored.
+    Determines the location for local data files by first
+    looking for an environment variable called NYC_SCHOOLS_DATA_DIR
 
-    If this ENV variable is not set, it looks for a local config file.
-
-    It looks for writeable directories in the following order:
-    1. `school-data` in the current directory
-    2. `platformdirs.site_data_dir`
-    3. `platformdirs.user_data_dir`
+    If this ENV variable is not set, 
+    data files are stored in a directory called `school-data` in the current
+    directory. If this directory does not exist, it is created.
 
     To see (and see how to change) these settings for your installation,
     run `python -m nycschools.dataloader`
 
     Returns:
         SimpleNamespace: a namespace object with the following attributes:
-            config_file: the path to the config file
             data_dir: the path to the data directory
-            urls: a dictionary of urls to download data from    
+            urls: a dictionary of urls to download data if the local
+                  cache should be re-built
     """
 
     env_dir = os.environ.get("NYC_SCHOOLS_DATA_DIR", None)
-    for path in __config_paths:
-        if os.path.exists(path):
-            with open(path,"r") as f:
-                config = json.loads(f.read())
-                config["urls"] = __read_urls()
-                # env var always overrides config file
-                if env_dir:
-                    config["data_dir"] = env_dir
-                return SimpleNamespace(**config)
-
-
-    # create a new config in the user space
-    path = os.path.join(dirs.user_config_dir, __config_file)
-    print("config file path:", path)
-
-    # create the directory if it doesn't exist
-    # the directory might exist due to previous install
-    if not os.path.exists(os.path.dirname(path)):
-        os.makedirs(os.path.dirname(path))
+    local = os.path.join(".", "school-data")
+    config = {}
+    if env_dir:
+        config["data_dir"] = env_dir
+    else:
+        config["data_dir"] = local
     
-    
-    config = {
-        "config_file": path,
-        "data_dir": find_data_dir(__data_paths, env_dir)
-    }
-    with open(path,"w") as f:
-        f.write(json.dumps(config, indent=2))
-
-    config["urls"] = __read_urls()
+    if not os.path.exists(config["data_dir"]):
+        os.mkdir(config["data_dir"])
+            
+    config["urls"] = __read_urls__()
     return SimpleNamespace(**config)
 
-
-def find_data_dir(data_paths, env_dir):
-    """Finds a writeable data directory"""
-    # if ther's a local data dir with data files in it
-    # use that first
-    
-    local = os.path.join(".", "school-data")
-    if env_dir:
-        data_paths = [env_dir] + data_paths
-    
-    data_paths.append(local)
-
-    for path in data_paths:
-        if check_write_edit_delete(path):
-            return path
-
-    return None
-
-
-def check_write_edit_delete(path):
-    """Checks if path can be a suitable data dir"""
-
-    print("Checking data dir at:", path)
-    if os.path.exists(path):
-        files = os.listdir(path)
-        if len(files) > 0:
-            # if the path exists and is not empty, assume it works
-            return True
-    else:
-        try:
-            print("trying to make", path)
-            os.makedirs(path)
-        except:
-            return False
-
-
-    f = os.path.join(path, ".tmp")
-    msg = "testing data dir"
-    with open(f, "w") as tmp:
-        try:
-            tmp.write(msg)
-        except:
-            return False
-
-    with open(f, "r") as tmp:
-        try:
-            assert tmp.read() == msg
-        except:
-            return False
-
-    with open(f, "a") as tmp:
-        try:
-            tmp.write("\nmore data")
-        except:
-            return False
-    try:
-        os.remove(f)
-    except:
-        return False
-
-    return True
 
 config = get_config()
--- test/test_config.py
+++ test/test_config.py
@@ -3,6 +3,5 @@ from nycschools import config
 
 def test_get_config():
     """Get the current/default configuration."""
-    assert config.config_file is not None
     assert config.data_dir is not None
     assert config.urls is not None
