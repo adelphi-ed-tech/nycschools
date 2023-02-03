9fa9be9167cc9352f1c1af80158ae50394e2cd4b
mxc <matt@curinga.com>
2023-02-02 19:56:43 -0500

    added tasks to build and push to pypi

--- tasks.py
+++ tasks.py
@@ -1,10 +1,21 @@
 import os
+import toml
+
 from nycschools import config
 from invoke import task
+
+
+def get_project_config():
+    config = {}
+    with open('pyproject.toml', 'r') as f:
+        config = toml.load(f)
+    return config["project"]
 
 @task
 def archive(c):
     """Create a .7z archive of all of the files in the data directory."""
+    
+    print("Creating a .7z archive of all of the files in the data directory.")
 
     data_dir = os.path.abspath(config.  data_dir)
     filename = config.urls["school-data-archive"].filename
@@ -17,7 +28,21 @@ def archive(c):
 @task
 def build(c):
     """Build the package."""
+    project = get_project_config()
+    print(f"building {project['name']} v{project['version']} ")
+    c.run("rm -rf dist")
     c.run("python -m build")
+
+@task
+def push(c, test=True):
+    """Build the package."""
+    if test:
+        print("pushing to testpypi")
+        c.run("twine upload --repository testpypi dist/*")
+    else:
+        print("pushing to pypi. this is NOT a drill.")
+        c.run("twine upload dist/*")
+    
 
 @task
 def test(c, opt=""):
@@ -28,5 +53,4 @@ def test(c, opt=""):
 def docs(c):
     """Build the documentation with sphynx."""
     c.run("sphinx-apidoc nycschools -o docs-source/api")
-    # c.run("rsync -r docs-source/res docs/res")
     c.run("sphinx-build -b html docs-source docs")
