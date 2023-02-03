0886c0fd7c6a2d629be45ceb4d019e8dc2f16b9f
mxc <matt@curinga.com>
2023-02-02 19:56:56 -0500

    updated build data

--- setup.py
+++ setup.py
@@ -7,53 +7,6 @@ here = pathlib.Path(__file__).parent.resolve()
 long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
-    name='nycschools',
-    version='0.1.0',
     packages=['nycschools'],
-    description="Tools to work with open data about New York City public schools",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/adelphi-ed-tech/nycschools",
-    author="Matthew X. Curinga",
-    author_email="matt@curinga.com",
-    license='OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Education',
-        'Intended Audience :: Science/Research',
-        'Natural Language :: English',
-        'Framework :: Jupyter',
-        'Topic :: Education',
-    ],
-    dependencies = [
-        "arrow",
-        "folium",
-        "geopandas",
-        "importlib",
-        "ipython",
-        "matplotlib",
-        "networkx",
-        "numpy",
-        "pandas",
-        "seaborn",
-        "wget",
-        "py7zr",
-        "python-Levenshtein",
-        "thefuzz"
-    ],
-    keywords="opendata, schools, newyorkcity",
-    python_requires=">=3.7",
-    project_urls={
-        "Bug Reports": "https://github.com/adelphi-ed-tech/nycschools/issues",
-        "Source": "https://github.com/adelphi-ed-tech/nycschools"
-    },
 
 )
