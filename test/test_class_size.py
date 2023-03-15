bb3b542db47e47ce0b0a98d5c2c560e760305208
mxc <matt@curinga.com>
2023-03-15 15:12:49 -0400

    test/
    added unit tests for cep downloads (shel), budgets, and class size

---
+++ test/test_class_size.py
@@ -0,0 +1,25 @@
+from nycschools import class_size
+
+
+def test_get_class_size():
+    df = class_size.get_class_size()
+    cols = {'ay', 'dbn', 'grade', 'students_n', 'classes_n', 'avg_class_size',
+            'min_class_size', 'max_class_size', 'dept', 'subject'}
+    assert cols.issubset(df.columns)
+    assert df.ay.nunique() >= 2
+
+
+def test_load_class_size():
+    df = class_size.load_class_size()
+
+    cols = {'dbn', 'school_name', 'grade', 'program_type', 'students_n',
+       'classes_n', 'avg_class_size', 'min_class_size', 'max_class_size',
+       'dept', 'subject', 'ay'}
+    assert cols.issubset(df.columns)
+    assert df.ay.nunique() >= 2
+
+def test_load_ptr():
+    df = class_size.load_ptr()
+    cols = {'dbn', 'school_name', 'ptr', 'ay'}
+    assert cols.issubset(df.columns)
+
